e346a6bb411423b9f8697de7b7da3f3a69ed186b
dima-frost <frostdimonlex@gmail.com>
2021-05-04 14:11:33 +0300

    add security group rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,12 @@ output "ec2_endpoint" {
   description = "public ip EC2 instance"
   value       = aws_instance.final-project.public_ip
 }
+
+resource "aws_security_group_rule" "ssh-pyapp" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_instance.final-project.vpc_security_group_ids
+}
