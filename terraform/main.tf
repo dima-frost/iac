b8efbee221655f6c3871f1f2b2f24145698d6a15
dima-frost <frostdimonlex@gmail.com>
2021-05-04 12:52:13 +0300

    edit terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2" {
-  source = "./modules/EC2"
-  name = "final-project"
+resource "aws_instance" "final-project" {
+  ami = "ami-013f17f36f8b1fefb"
+  instance_type = "t2.micro"
+  key_name = "itea"
+  tags = {
+    Name = "final-project"
+  }
+}
+
+output "ec2_endpoint" {
+  description = "public ip EC2 instance"
+  value       = aws_instance.final-project.public_ip
 }
--- terraform/modules/EC2/output.tf
+++
@@ -1,4 +0,0 @@
-output "ec2_endpoint" {
-  description = "public ip EC2 instance"
-  value       = aws_instance.final-project.public_ip
-}
